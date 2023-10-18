9300ff9cb3a75c3bfdbd5908efd22deab80c16ae
TylerSerrette <tylerserrette17@gmail.com>
2023-10-17 22:23:27 -0500

    lives and score added

--- Tutorials/Asteroids/Asteroids.py
+++ Tutorials/Asteroids/Asteroids.py
@@ -1,7 +1,11 @@
 import pygame
-from pygame import Vector2
+from pygame import MOUSEBUTTONDOWN, MOUSEMOTION, Vector2
 from pygame.transform import rotozoom
 import random
+from pygame.mixer import Sound
+
+asteroid_images = ['./Images/asteroid1.png',
+                   './Images/asteroid2.png', './Images/asteroid3.png',]
 
 
 def blit_rotated(position, image, forward, screen):
@@ -37,6 +41,7 @@ class Ship:
         self.bullets = []
         self.can_shoot = 0
         self.drift = (0, 0)
+        self.shoot = Sound("./Sounds/shoot.wav")
 
     def update(self):
         is_key_pressed = pygame.key.get_pressed()
@@ -44,14 +49,15 @@ class Ship:
             self.position += self.forward
             self.drift = (self.drift + self.forward) / 1.5
         if is_key_pressed[pygame.K_LEFT]:
-            self.forward = self.forward.rotate(-1)
+            self.forward = self.forward.rotate(-2)
         if is_key_pressed[pygame.K_RIGHT]:
-            self.forward = self.forward.rotate(1)
+            self.forward = self.forward.rotate(2)
         if is_key_pressed[pygame.K_SPACE] and self.can_shoot == 0:
             # Giving it Vector2(self.position) creates a new position variable for the bullets
             # without it, we are passing/updating the ships position.
             self.bullets.append(
                 Bullet(Vector2(self.position), self.forward * 10))
+            self.shoot.play()
             # can_shoot = 500ms
             self.can_shoot = 500
 
@@ -87,14 +93,14 @@ class Bullet:
 
 
 # Asteroid class
-
-
 class Asteroid:
-    def __init__(self, position):
+    def __init__(self, position, size):
         self.position = Vector2(position)
         self.velocity = Vector2(random.randint(-3, 3), random.randint(-3, 3))
-        self.image = pygame.image.load("./Images/asteroid1.png")
+        self.image = pygame.image.load(asteroid_images[size])
         self.radius = self.image.get_width() // 2
+        self.explode = Sound("./Sounds/explode.mp3")
+        self.size = size
 
     def update(self):
         self.position += self.velocity
@@ -109,6 +115,7 @@ class Asteroid:
         # if the asteroids position has a distance to the bullets position that is
         # <= the radius of the asteroid, then the bullet has hit the asteroid.
         if self.position.distance_to(position) <= self.radius:
+            self.explode.play()
             return True
         return False
 
@@ -121,83 +128,247 @@ background = pygame.image.load("./Images/space.png")
 game_over = False
 
 # Creating ship, giving it starting position
-ship = Ship((100, 700))
+ship = Ship((screen.get_width()//2, screen.get_height()//2))
 
 # Creating Asteroids
 # Create the emtpy list of asteroids and create a loop to make them.
 # Range being the number of asteroids you want to create.
 asteroids = []
-for i in range(10):
+for i in range(3):
+    posx, posy = (random.randint(0, screen.get_width()),
+                  random.randint(0, screen.get_height()))
+    asteroid_position = (posx, posy)
+    asteroid_direction_away_from_screen_center = (
+        (screen.get_width()/2 - posx) * 0.8, (screen.get_height()/2-posy)*0.8)
+    asteroid_position += asteroid_direction_away_from_screen_center
+    asteroids.append(
+        Asteroid((asteroid_position[0], asteroid_position[1]), 0))
     # Asteroids appear in random positions on the screen.
-    asteroids.append(Asteroid((random.randint
-                               (0, screen.get_width()), random.randint(0, screen.get_height()))))
+    # asteroids.append(Asteroid((random.randint
+    #                            (0, screen.get_width()), random.randint(0, screen.get_height()))))
 
 # Out of bounds setting container
 out_of_bounds = [-150, -150, 950, 950]
 
+# Lose screen
+font = pygame.font.Font("./Fonts/AlienFont.ttf", 80)
+text_loser = font.render("You Lost!", True, (255, 255, 255))
+text_loser_position = ((screen.get_width() - text_loser.get_width()) //
+                       2, (screen.get_height() - text_loser.get_height())//2)
+# Win screen
+font2 = pygame.font.Font("./Fonts/AlienFont.ttf", 80)
+text_winner = font2.render("You Won!", True, (255, 255, 255))
+text_winner_position = ((screen.get_width() - text_winner.get_width()) //
+                        2, (screen.get_height() - text_winner.get_height())//2)
+# Score display
+score = 0
+score_font = pygame.font.Font("./Fonts/AlienScore.ttf", 35)
+# score_text = score_font.render(f'{score}', True, (255, 255, 255))
+score_text_position = (10, 10)
+
+# Lives display
+lives = 3
+lives_font = pygame.font.Font("./Fonts/AlienScore.ttf", 35)
+lives_text_position = (10, 50)
+life_timer = 0
+
+# Quit button
+quit_font = pygame.font.Font("./Fonts/AlienFont.ttf", 50)
+quit_hovered = False
+quit_text = quit_font.render(
+    'Quit?', True, ((255, 255, 255) if quit_hovered == False else (0, 0, 0)))
+quit_text_rect = quit_text.get_rect()
+quit_text_rect.center = ((screen.get_width(
+) - quit_text.get_width()) // 2, (screen.get_height() - text_loser.get_height()) // 2 + 100,)
+
+# Try again button
+try_again_font = pygame.font.Font("./Fonts/AlienFont.ttf", 50)
+try_again_hovered = False
+try_again_text = try_again_font.render(
+    'Try again?', True, ((255, 255, 255) if quit_hovered == False else (0, 0, 0)))
+try_again_rect = try_again_text.get_rect()
+try_again_rect.center = ((screen.get_width(
+) - try_again_text.get_width()) // 2, (screen.get_height() - quit_text.get_height()) // 2 + 150,)
+
+
 clock = pygame.time.Clock()
-
-while not game_over:
-
-    clock.tick(55)
+quitting = False
+running = True
+winner = False
+loser = False
+while quitting == False:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
+            quitting = True
 
-    # Fill screen with background image to have sprite trails cleaned up on renders.
-    # It is an image that is blit to the screen.
-    screen.blit(background, (0, 0))
+        if event.type == MOUSEMOTION:
+            x, y = event.pos
+            # Quit
+            if quit_text_rect.collidepoint(x, y):
+                quit_hovered = True
+            else:
+                quit_hovered = False
+            # Try again
+            if try_again_rect.collidepoint(x, y):
+                try_again_hovered = True
+            else:
+                try_again_hovered = False
 
-    if ship is None:
-        pygame.display.update()
-        continue
+        if event.type == MOUSEBUTTONDOWN and event.button == 1:
+            if quit_hovered:
+                game_over = True
+                running = False
+                quitting = True
+            if try_again_hovered:
+                game_over = False
+                running = True
+                quitting = False
 
-    # Running ships update and draw method.
-    ship.update()
-    ship.draw(screen)
+    if running:
+        while game_over == False:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    game_over = True
+                    running = False
+                    quitting = True
 
-    # Running asteroids update and draw method.
-    for asteroid in asteroids:
-        asteroid.update()
-        asteroid.draw(screen)
-        if asteroid.hit(ship.position):
-            ship = None
-            break
+                if event.type == MOUSEMOTION:
+                    x, y = event.pos
+                    # Quit
+                    if quit_text_rect.collidepoint(x, y):
+                        quit_hovered = True
+                    else:
+                        quit_hovered = False
+                    # Try again
+                    if try_again_rect.collidepoint(x, y):
+                        try_again_hovered = True
+                    else:
+                        try_again_hovered = False
 
-    if ship is None:
-        continue
+                if event.type == MOUSEBUTTONDOWN and event.button == 1:
+                    if quit_hovered:
+                        game_over = True
+                        running = False
+                        quitting = True
+                    if try_again_hovered:
+                        game_over = False
+                        running = True
+                        quitting = False
 
-    deadbullets = []
-    deadasteroids = []
+            clock.tick(55)
 
-    # Running bullets update and draw method.
-    for bullet in ship.bullets:
-        bullet.update()
-        bullet.draw(screen)
+            # Fill screen with background image to have sprite trails cleaned up on renders.
+            # It is an image that is blit to the screen.
+            screen.blit(background, (0, 0))
 
-        # If bullet goes out of bounds it gets put in dead bullet list
-        if bullet.position.x < out_of_bounds[0] or \
-            bullet.position.x > out_of_bounds[2] or \
-                bullet.position.y < out_of_bounds[1] or \
-        bullet.position.y > out_of_bounds[3]:
-            if not deadbullets.__contains__(bullet):
-                deadbullets.append(bullet)
+            if ship is None:
+                game_over = True
+                running = False
+                loser = True
+                winner = False
+                continue
+            # if ship is None:
+            #     screen.blit(text_loser, text_loser_position)
+            #     screen.blit(quit_text, quit_text_rect.center)
+            #     screen.blit(try_again_text, try_again_rect.center)
+            #     pygame.display.update()
+            #     continue
 
-        for asteroid in asteroids:
-            # if a hits b then a and be need to be destroyed.
-            if asteroid.hit(bullet.position):
-                if not deadbullets.__contains__(bullet):
-                    deadbullets.append(bullet)
-                if not deadasteroids.__contains__(asteroid):
-                    deadasteroids.append(asteroid)
+            if len(asteroids) == 0:
+                game_over = True
+                running = False
+                winner = True
+                loser = False
+                continue
+                # screen.blit(text_winner, text_winner_position)
+                # screen.blit(quit_text, quit_text_rect.center)
+                # screen.blit(try_again_text, try_again_rect.center)
+                # pygame.display.update()
+                # continue
 
-    for deadbullet in deadbullets:
-        ship.bullets.remove(deadbullet)
+            # Running ships update and draw method.
+            ship.update()
+            ship.draw(screen)
 
-    for deadasteroid in deadasteroids:
-        asteroids.remove(deadasteroid)
+            # Running asteroids update and draw method.
+            for asteroid in asteroids:
+                asteroid.update()
+                asteroid.draw(screen)
+                if asteroid.hit(ship.position):
+                    if lives == 0:
+                        ship = None
+                        break
+                    elif life_timer == 0:
+                        life_timer = 1500
+                        lives -= 1
+                        break
+                    break
 
-    pygame.display.update()
+            # Timer for life counter so 1 asteroid wont wipe out the ships life in a second.
+            if life_timer > 0:
+                life_timer -= clock.get_time()
+            else:
+                life_timer = 0
+
+            if ship is None:
+                continue
+
+            deadbullets = []
+            deadasteroids = []
+
+            # Running bullets update and draw method.
+            for bullet in ship.bullets:
+                bullet.update()
+                bullet.draw(screen)
+
+                # If bullet goes out of bounds it gets put in dead bullet list
+                if bullet.position.x < out_of_bounds[0] or \
+                    bullet.position.x > out_of_bounds[2] or \
+                        bullet.position.y < out_of_bounds[1] or \
+                bullet.position.y > out_of_bounds[3]:
+                    if not deadbullets.__contains__(bullet):
+                        deadbullets.append(bullet)
+
+                for asteroid in asteroids:
+                    # if a hits b then a and be need to be destroyed.
+                    if asteroid.hit(bullet.position):
+                        score += 1
+                        if not deadbullets.__contains__(bullet):
+                            deadbullets.append(bullet)
+                        if not deadasteroids.__contains__(asteroid):
+                            deadasteroids.append(asteroid)
+
+            for deadbullet in deadbullets:
+                ship.bullets.remove(deadbullet)
+
+            for deadasteroid in deadasteroids:
+                if deadasteroid.size < 2:
+                    asteroids.append(
+                        Asteroid(deadasteroid.position, deadasteroid.size + 1))
+                    asteroids.append(
+                        Asteroid(deadasteroid.position, deadasteroid.size + 1))
+                asteroids.remove(deadasteroid)
+
+            # Display score
+            score_text = score_font.render(
+                f'Score: {score}', True, (255, 255, 255))
+            screen.blit(score_text, score_text_position)
+            # Display lives
+            lives_text = lives_font.render(
+                f'Lives: {lives}', True, (255, 255, 255))
+            screen.blit(lives_text, lives_text_position)
+
+            pygame.display.update()
+    else:
+        if loser:
+            screen.blit(text_loser, text_loser_position)
+            screen.blit(quit_text, quit_text_rect.center)
+            pygame.display.update()
+        elif winner:
+            screen.blit(text_winner, text_winner_position)
+            screen.blit(quit_text, quit_text_rect.center)
+            pygame.display.update()
 
 pygame.quit()
